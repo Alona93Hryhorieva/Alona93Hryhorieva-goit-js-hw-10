4da79000c9bca67fbb6cb11f42870834fa109ebf
Alona93Hryhorieva <151534732+Alona93Hryhorieva@users.noreply.github.com>
2024-05-06 21:35:33 +0300

    21.35

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -7,16 +7,6 @@ import iziToast from "izitoast";
 // Додатковий імпорт стилів
 import "izitoast/dist/css/iziToast.min.css";
 
-const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    console.log(selectedDates[0]);
-  },
-};
-
 const startBtn = document.querySelector('[data-start]');
 const inputPicker = document.querySelector('#datetime-picker');
 const timerFields = {
@@ -26,32 +16,71 @@ const timerFields = {
     seconds: document.querySelector('[data-seconds]'),
 };
 
-flatpickr(inputPicker, options);
-
 function addLeadingZero(value) {
   return value.toString().padStart(2, '0');
 }
 
-let userSelectedDate = null;
-function onClose(selectedDates) {
-    // userSelectedDate = selectedDates[0];
-    if (selectedDates[0] < new Date()) {
-        startBtn.disabled = false;
-        iziToast.error({
-            title: "Error",
-            message: "Please choose a date in the future",
-        });
-    } else {
-        startBtn.disabled = true;
+let userSelectedDate;
+let timerInterval;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    //   console.log(selectedDates[0]);
+      userSelectedDate = selectedDates[0];
+       if (userSelectedDate < new Date()) {
+         startBtn.disabled = true;
+         iziToast.error({
+             title: "Error",
+             message: "Please choose a date in the future",
+            });
+        } else {
+         startBtn.disabled = false;
+        }
+    },
+};
+
+flatpickr(inputPicker, options);
+
+startBtn.addEventListener('click', () => {
+    if (!userSelectedDate || userSelectedDate < new Date()) {
+        return
+    };
+
+    startBtn.disabled = true;
+    inputPicker.disabled = true;
+
+    const startTime = new Date();
+    const endTime = userSelectedDate.getTime();
+
+    function updateTimer() {
+        const currentTime = new Date();
+        const timeRemaining = endTime - currentTime.getTime();
+
+        if (timeRemaining <= 0) {
+         clearInterval(timerInterval);
+         inputPicker.disabled = false;
+         timerFields.days.textContent = '00';
+         timerFields.hours.textContent = '00';
+         timerFields.minutes.textContent = '00';
+         timerFields.seconds.textContent = '00';
+         return;
+        }
+        
+        const { days, hours, minutes, seconds } = convertMs(timeRemaining);
+
+        timerFields.days.textContent = addLeadingZero(days.toString());
+      timerFields.hours.textContent = addLeadingZero(hours.toString());
+      timerFields.minutes.textContent = addLeadingZero(minutes.toString());
+      timerFields.seconds.textContent = addLeadingZero(seconds.toString());
     }
-}
-
-
-
-
-
-
-
+    
+    timerInterval = setInterval(updateTimer, 1000);
+    updateTimer(); 
+})
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -71,7 +100,6 @@ function convertMs(ms) {
 
   return { days, hours, minutes, seconds };
 }
-
 // console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
 // console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-//  console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+// console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
