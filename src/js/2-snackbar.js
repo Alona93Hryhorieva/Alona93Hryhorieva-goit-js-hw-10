33ca07440770696dc807ebd0680a17804d1bc543
Alona93Hryhorieva <151534732+Alona93Hryhorieva@users.noreply.github.com>
2024-05-07 12:03:56 +0300

    12/03

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -3,3 +3,6 @@ import iziToast from "izitoast";
 // Додатковий імпорт стилів
 import "izitoast/dist/css/iziToast.min.css";
 
+const intupDelay = document.querySelector("[type=number]");
+const inputRadio = document.querySelector("[type=radio]");
+const btnSubmit = document.querySelector("[type=submit]");
