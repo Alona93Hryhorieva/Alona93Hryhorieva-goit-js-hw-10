7b56d9f8bdfcb80ab43463029a0de10e36753b7f
Alona93Hryhorieva <151534732+Alona93Hryhorieva@users.noreply.github.com>
2024-05-07 09:04:12 +0000

    Deploying to gh-pages from @ Alona93Hryhorieva/goit-js-hw-10@33ca07440770696dc807ebd0680a17804d1bc543 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */import"./assets/vendor-77e16229.js";document.querySelector("[type=number]");document.querySelector("[type=radio]");document.querySelector("[type=submit]");
+//# sourceMappingURL=commonHelpers2.js.map
